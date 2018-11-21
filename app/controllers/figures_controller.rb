15494fa80eed5aa679fe08c514f2d49392b29b65
Skyler Phillips <skylerbae@Skylers-MacBook-Pro.local>
2018-11-21 16:09:38 -0500

    second commit

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,39 @@
+
+
 class FiguresController < ApplicationController
-  # add controller methods
+  get '/figures' do
+    @figures = Figure.all
+    erb :"../views/figures/index"
+  end
+
+  get'/figures/new' do
+    @titles = Title.all
+    @landmarks = Landmark.all
+    erb :"../views/figures/new"
+  end
+
+  get '/figures/:id' do
+    @figure = Figure.find(param[:id])
+     erb :show
+  end
+
+  get 'figures/:id/edit' do
+    @figure = Figure.find(param[:id])
+    erb  :edit
+  end
+
+  post '/figures' do
+    binding.pry
+    @figure = Figure.create(params[:figure])
+    @landmark = Landmark.create(params[:landmark])
+    @landmark.figure_id = @figure.id
+    @title = Title.create(params[:title])
+    redirect "/figures/#{@figure.id}"
+  end
+
+  patch 'figures/:id' do
+    @figure = Figure.update(params[:figure])
+    redirect "/figures/#{@figure.id}"
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -13,13 +13,13 @@
 
 ActiveRecord::Schema.define(version: 20181121190222) do
 
-  create_table "figures", force: :cascade do |t|
-    t.string "name"
-  end
-
-  create_table "figuretitles", force: :cascade do |t|
+  create_table "figure_titles", force: :cascade do |t|
     t.integer "title_id"
     t.integer "figure_id"
+  end
+
+  create_table "figures", force: :cascade do |t|
+    t.string "name"
   end
 
   create_table "landmarks", force: :cascade do |t|
